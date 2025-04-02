75e9439042a8df7c69c34d7f98e708863a091bb6
1HoWK <howaikit01@gmail.com>
2025-04-02 12:32:19 +0800

    Reupload

---
+++ src/coding-challenges/twoSum.js
@@ -0,0 +1,22 @@
+var twoSum = function(nums, target) {
+    // Method 1 : nested for loops (brute force) O(n2)
+    // Method 2 : using hashmap
+
+    // Create a Map
+    // keep track of what we have checked
+    const map = new Map();
+
+    // for loop to iterate each element
+    for (let i = 0; i < nums.length; i++){
+        const difference = target - nums[i];
+        // check if it exists
+        if(map.has(difference)){
+            return [map.get(difference), i];
+        }
+
+        // add element
+        map.set(nums[i], i);
+    }
+
+    return [];
+};
