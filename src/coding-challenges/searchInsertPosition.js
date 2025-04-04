e3c43d37ad1a206bc8bece89d8b08f578ac22378
1HoWK <howaikit01@gmail.com>
2025-04-04 23:56:57 +0800

    Search Insert Position

---
+++ src/coding-challenges/searchInsertPosition.js
@@ -0,0 +1,17 @@
+var searchInsert = function (nums, target) {
+  let l = 0;
+  let r = nums.length - 1;
+
+  while (l <= r) {
+    let mid = Math.floor((l + r) / 2);
+        
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
+        r = mid - 1; // -1 to avoid repeat
+    } else {
+        l = mid + 1;
+    }
+  }
+  return l;
+};
